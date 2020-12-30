8b4768625079dedeb0a58041b32348ee40879619
Phil Robinson <philip.robinson84@gmail.com>
2020-12-30 14:47:59 +0000

    Added CPUTemp. Only tested on Pi4.

--- home-assistant-sysmon.py
+++ home-assistant-sysmon.py
@@ -21,10 +21,27 @@ def registerSensors():
     config = {"name": f"{room} Disk Usage", "device_class": "power_factor", "unit_of_measurement": "%", "state_topic": f"homeassistant/sensor/{room.lower()}/Disk"}
     print(f"Registering sensor: {config}")
     client.publish(f"homeassistant/sensor/{room.lower()}/Disk/config", json.dumps(config))
+    config = {"name": f"{room} CPU Temperature", "device_class": "temperature", "unit_of_measurement": "°C", "state_topic": f"homeassistant/sensor/{room.lower()}/CPUTemp"}
+    print(f"Registering sensor: {config}")
+    client.publish(f"homeassistant/sensor/{room.lower()}/CPUTemp/config", json.dumps(config))
 
 registerSensors()
 while True:
-    client.publish(f"homeassistant/sensor/{room.lower()}/CPU", psutil.cpu_percent())
-    client.publish(f"homeassistant/sensor/{room.lower()}/RAM", psutil.virtual_memory().percent)
-    client.publish(f"homeassistant/sensor/{room.lower()}/Disk", psutil.disk_usage('/').percent)
+    temps = psutil.sensors_temperatures()
+    cpuTemp = 0
+    for name, entries in temps.items():
+        if name == 'cpu_thermal':
+            for entry in entries:
+                cpuTemp = entry.current
+    data = {
+            'CPU':psutil.cpu_percent(),
+            'RAM':psutil.virtual_memory().percent,
+            'Disk':psutil.disk_usage('/').percent,
+            'CPUTemp':cpuTemp,
+           }
+    print(data)
+    client.publish(f"homeassistant/sensor/{room.lower()}/CPU", data['CPU'])
+    client.publish(f"homeassistant/sensor/{room.lower()}/RAM", data['RAM'])
+    client.publish(f"homeassistant/sensor/{room.lower()}/Disk", data['Disk'])
+    client.publish(f"homeassistant/sensor/{room.lower()}/CPUTemp", data['CPUTemp'])
     sleep(5)
